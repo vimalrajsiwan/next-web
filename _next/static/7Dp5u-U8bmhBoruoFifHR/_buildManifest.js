ec782ceedaae515002aa9fb20750c5934990e765
Vimal Raj <vimal.raj.siwan.se@gmail.com>
2025-10-20 20:53:57 +0530

    Updates

---
+++ _next/static/7Dp5u-U8bmhBoruoFifHR/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ce3c1be655149c50.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/194081acd06ac539.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/28ef11e0e926e322.js",
+  "static/chunks/turbopack-575f60062eb17e35.js"
+])
