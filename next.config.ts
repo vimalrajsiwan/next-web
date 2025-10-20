2dfaeb005742a2633c87b5e4ac6fef7ce47b27cf
Vimal Raj <vimal.raj.siwan.se@gmail.com>
2025-10-20 18:10:19 +0530

    build related changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/next-web', // 👈 Replace with your repo name
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
