59343f29d66c03599a8077101ab7db8f2e47e03f
Vimal Raj <vimal.raj.siwan.se@gmail.com>
2025-10-20 21:30:21 +0530

    Updates

--- _next/static/chunks/194081acd06ac539.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/28ef11e0e926e322.js",
-  "static/chunks/turbopack-575f60062eb17e35.js"
-])
--- _next/static/chunks/ce3c1be655149c50.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/28ef11e0e926e322.js",
-  "static/chunks/turbopack-6644505f28730be7.js"
-])
