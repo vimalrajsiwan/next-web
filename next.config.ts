af08f0481cb7e340e9effa7f48d570362a3cf416
Vimal Raj <vimal.raj.siwan.se@gmail.com>
2025-11-04 16:40:50 +0530

    updated next config for email

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  // basePath: '/next-web', // 👈 Replace with your repo name
-  // assetPrefix: '/next-web/', // 👈 also add this line
-  //  basePath: "", // leave empty since you are using a custom domain like vizlyx.com
-  // assetPrefix: "./", // important for relative asset paths
-  //  basePath: '/next-web',
-  // assetPrefix: '/next-web/',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
